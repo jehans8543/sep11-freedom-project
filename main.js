ea3ff244a9bdc324a974e21b4547dc00ad8ddcfe
jehans8543 <113617085+jehans8543@users.noreply.github.com>
2026-05-14 01:03:16 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const scene = new THREE.Scene();
+import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
 
+const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
 camera.position.z = 5;
 
